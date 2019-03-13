d898e20213bc55dde1cf4b551d847899fd754c79
Dan Høegh <dan.hoegh@gmail.com>
2019-03-13 09:53:30 +0100

    Comments and tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,25 +34,6 @@ const data = {
             }
         },
         {
-            "id": 2,
-            "metaData":
-            {
-                "name": "About us",
-                "title": "About us",
-                "template": "page",
-                "visible": true
-            },
-            "content": {
-                "header": "Info about us",
-                "image": "assets/images/img2.jpg",
-                "text":
-                    "<p>Morbi porta maximus faucibus. Nulla mollis mollis ultricies. Nunc vel lectus mollis, faucibus tellus non, consequat nulla. Donec et rhoncus velit. Nam commodo placerat mauris, ut sagittis ipsum consectetur vestibulum. Duis quis turpis vel nisi venenatis imperdiet. Cras pretium semper scelerisque. Morbi lobortis fermentum massa, ac ultricies ex interdum sed. Mauris sit amet nulla venenatis, tincidunt dolor non, feugiat elit. Integer pellentesque ut diam at suscipit. Phasellus eget dolor euismod, varius orci nec, porta ligula. Sed convallis turpis mauris, at pellentesque nisl dictum sed.</p>" +
-                    "<p>Aliquam sit amet sem ac ligula sodales suscipit vel in nulla. Vivamus sit amet faucibus justo, dictum vehicula urna. Cras dui orci, consectetur non risus nec, tincidunt rhoncus metus. Morbi et tempus elit. Fusce venenatis quam et metus malesuada, porttitor congue metus posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse sit amet venenatis massa. Fusce nec odio porttitor, eleifend erat eget, scelerisque purus. Nulla facilisi. Integer viverra vulputate consectetur. Duis sit amet luctus quam. Mauris bibendum, metus sed lobortis imperdiet, nisi sapien aliquam lacus, ac elementum sapien tortor sit amet diam. Nullam et mi libero.</p>" +
-                    "<p>Suspendisse nec fermentum ligula. Cras sagittis tristique neque, ut sollicitudin nunc lacinia vel. Proin volutpat consequat accumsan. Etiam vel justo turpis. Nullam finibus, erat eget cursus tempus, risus libero hendrerit sem, sed aliquam erat mi in elit. Vestibulum aliquam mi eros, eu tempus sem sagittis a. Nullam aliquam libero a sem placerat, eget rutrum nisl feugiat. Quisque in maximus sem, vel rhoncus orci. Donec condimentum leo erat, at rutrum nunc egestas non. Nullam consectetur dictum ante nec euismod. In nibh dui, euismod a rhoncus sed, congue vitae libero.</p>" +
-                    "<p>Phasellus facilisis dignissim enim, pharetra facilisis lectus luctus ac. Phasellus at porttitor orci. Ut ultricies massa risus, eu feugiat arcu bibendum ut. Etiam eu semper orci. Ut at accumsan sem. Praesent ut dui volutpat orci interdum tincidunt. Quisque tincidunt, nulla facilisis gravida varius, velit urna pretium tortor, eu viverra eros arcu vel ex. Duis lobortis scelerisque tellus, vitae dignissim erat eleifend sed.</p>"
-            }
-        },
-        {
             "id": 3,
             "metaData":
             {
@@ -64,6 +45,25 @@ const data = {
             "content": {
                 "header": "Info about you",
                 "image": "assets/images/img2.jpg",
+                "text":
+                    "<p>Morbi porta maximus faucibus. Nulla mollis mollis ultricies. Nunc vel lectus mollis, faucibus tellus non, consequat nulla. Donec et rhoncus velit. Nam commodo placerat mauris, ut sagittis ipsum consectetur vestibulum. Duis quis turpis vel nisi venenatis imperdiet. Cras pretium semper scelerisque. Morbi lobortis fermentum massa, ac ultricies ex interdum sed. Mauris sit amet nulla venenatis, tincidunt dolor non, feugiat elit. Integer pellentesque ut diam at suscipit. Phasellus eget dolor euismod, varius orci nec, porta ligula. Sed convallis turpis mauris, at pellentesque nisl dictum sed.</p>" +
+                    "<p>Aliquam sit amet sem ac ligula sodales suscipit vel in nulla. Vivamus sit amet faucibus justo, dictum vehicula urna. Cras dui orci, consectetur non risus nec, tincidunt rhoncus metus. Morbi et tempus elit. Fusce venenatis quam et metus malesuada, porttitor congue metus posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse sit amet venenatis massa. Fusce nec odio porttitor, eleifend erat eget, scelerisque purus. Nulla facilisi. Integer viverra vulputate consectetur. Duis sit amet luctus quam. Mauris bibendum, metus sed lobortis imperdiet, nisi sapien aliquam lacus, ac elementum sapien tortor sit amet diam. Nullam et mi libero.</p>" +
+                    "<p>Suspendisse nec fermentum ligula. Cras sagittis tristique neque, ut sollicitudin nunc lacinia vel. Proin volutpat consequat accumsan. Etiam vel justo turpis. Nullam finibus, erat eget cursus tempus, risus libero hendrerit sem, sed aliquam erat mi in elit. Vestibulum aliquam mi eros, eu tempus sem sagittis a. Nullam aliquam libero a sem placerat, eget rutrum nisl feugiat. Quisque in maximus sem, vel rhoncus orci. Donec condimentum leo erat, at rutrum nunc egestas non. Nullam consectetur dictum ante nec euismod. In nibh dui, euismod a rhoncus sed, congue vitae libero.</p>" +
+                    "<p>Phasellus facilisis dignissim enim, pharetra facilisis lectus luctus ac. Phasellus at porttitor orci. Ut ultricies massa risus, eu feugiat arcu bibendum ut. Etiam eu semper orci. Ut at accumsan sem. Praesent ut dui volutpat orci interdum tincidunt. Quisque tincidunt, nulla facilisis gravida varius, velit urna pretium tortor, eu viverra eros arcu vel ex. Duis lobortis scelerisque tellus, vitae dignissim erat eleifend sed.</p>"
+            }
+        },
+        {
+            "id": 9,
+            "metaData":
+            {
+                "name": "Another page",
+                "title": "Another page",
+                "template": "page",
+                "visible": true
+            },
+            "content": {
+                "header": "Another page",
+                "image": "assets/images/img1.jpg",
                 "text":
                     "<p>Morbi porta maximus faucibus. Nulla mollis mollis ultricies. Nunc vel lectus mollis, faucibus tellus non, consequat nulla. Donec et rhoncus velit. Nam commodo placerat mauris, ut sagittis ipsum consectetur vestibulum. Duis quis turpis vel nisi venenatis imperdiet. Cras pretium semper scelerisque. Morbi lobortis fermentum massa, ac ultricies ex interdum sed. Mauris sit amet nulla venenatis, tincidunt dolor non, feugiat elit. Integer pellentesque ut diam at suscipit. Phasellus eget dolor euismod, varius orci nec, porta ligula. Sed convallis turpis mauris, at pellentesque nisl dictum sed.</p>" +
                     "<p>Aliquam sit amet sem ac ligula sodales suscipit vel in nulla. Vivamus sit amet faucibus justo, dictum vehicula urna. Cras dui orci, consectetur non risus nec, tincidunt rhoncus metus. Morbi et tempus elit. Fusce venenatis quam et metus malesuada, porttitor congue metus posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse sit amet venenatis massa. Fusce nec odio porttitor, eleifend erat eget, scelerisque purus. Nulla facilisi. Integer viverra vulputate consectetur. Duis sit amet luctus quam. Mauris bibendum, metus sed lobortis imperdiet, nisi sapien aliquam lacus, ac elementum sapien tortor sit amet diam. Nullam et mi libero.</p>" +
@@ -85,6 +85,25 @@ const data = {
                 "image": "assets/images/img5.jpg",
                 "text":
                     "<p>Morbi porta maximus faucibus. Nulla mollis mollis ultricies. Nunc vel lectus mollis, faucibus tellus non, consequat nulla. Donec et rhoncus velit. Nam commodo placerat mauris, ut sagittis ipsum consectetur vestibulum. Duis quis turpis vel nisi venenatis imperdiet. Cras pretium semper scelerisque. Morbi lobortis fermentum massa, ac ultricies ex interdum sed. Mauris sit amet nulla venenatis, tincidunt dolor non, feugiat elit. Integer pellentesque ut diam at suscipit. Phasellus eget dolor euismod, varius orci nec, porta ligula. Sed convallis turpis mauris, at pellentesque nisl dictum sed.</p>"
+            }
+        },
+        {
+            "id": 2,
+            "metaData":
+            {
+                "name": "About us",
+                "title": "About us",
+                "template": "page",
+                "visible": true
+            },
+            "content": {
+                "header": "Info about us",
+                "image": "assets/images/img2.jpg",
+                "text":
+                    "<p>Morbi porta maximus faucibus. Nulla mollis mollis ultricies. Nunc vel lectus mollis, faucibus tellus non, consequat nulla. Donec et rhoncus velit. Nam commodo placerat mauris, ut sagittis ipsum consectetur vestibulum. Duis quis turpis vel nisi venenatis imperdiet. Cras pretium semper scelerisque. Morbi lobortis fermentum massa, ac ultricies ex interdum sed. Mauris sit amet nulla venenatis, tincidunt dolor non, feugiat elit. Integer pellentesque ut diam at suscipit. Phasellus eget dolor euismod, varius orci nec, porta ligula. Sed convallis turpis mauris, at pellentesque nisl dictum sed.</p>" +
+                    "<p>Aliquam sit amet sem ac ligula sodales suscipit vel in nulla. Vivamus sit amet faucibus justo, dictum vehicula urna. Cras dui orci, consectetur non risus nec, tincidunt rhoncus metus. Morbi et tempus elit. Fusce venenatis quam et metus malesuada, porttitor congue metus posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse sit amet venenatis massa. Fusce nec odio porttitor, eleifend erat eget, scelerisque purus. Nulla facilisi. Integer viverra vulputate consectetur. Duis sit amet luctus quam. Mauris bibendum, metus sed lobortis imperdiet, nisi sapien aliquam lacus, ac elementum sapien tortor sit amet diam. Nullam et mi libero.</p>" +
+                    "<p>Suspendisse nec fermentum ligula. Cras sagittis tristique neque, ut sollicitudin nunc lacinia vel. Proin volutpat consequat accumsan. Etiam vel justo turpis. Nullam finibus, erat eget cursus tempus, risus libero hendrerit sem, sed aliquam erat mi in elit. Vestibulum aliquam mi eros, eu tempus sem sagittis a. Nullam aliquam libero a sem placerat, eget rutrum nisl feugiat. Quisque in maximus sem, vel rhoncus orci. Donec condimentum leo erat, at rutrum nunc egestas non. Nullam consectetur dictum ante nec euismod. In nibh dui, euismod a rhoncus sed, congue vitae libero.</p>" +
+                    "<p>Phasellus facilisis dignissim enim, pharetra facilisis lectus luctus ac. Phasellus at porttitor orci. Ut ultricies massa risus, eu feugiat arcu bibendum ut. Etiam eu semper orci. Ut at accumsan sem. Praesent ut dui volutpat orci interdum tincidunt. Quisque tincidunt, nulla facilisis gravida varius, velit urna pretium tortor, eu viverra eros arcu vel ex. Duis lobortis scelerisque tellus, vitae dignissim erat eleifend sed.</p>"
             }
         }
     ],
@@ -156,29 +175,58 @@ const data = {
     ]
 };
 
-// console.log(data);
+// output the JSON data object to console. 
+// This is only done so that we can click through it in the developer console. 
+// Not needed to make stuff work
+console.log(data); 
 
 window.onload = function(){
-    // get pageId from URL, if no pageID, then let it be 0
+    // get our current page id from the URL
     let pageId = getPageIdFromUrl();
+    
+    // call on the drawSite function, use pageId to specify which page we want to draw
     drawSite(pageId);
 }
 
+function getPageIdFromUrl(){
+    // get pageId from URL, if no pageId, then let it be 0
+    let pageId = 0;
+    let url = window.location.href; 
+    if(url.indexOf("pageId") > -1){ 
+        let ulrSplit = url.split("?pageId=");
+        pageId = ulrSplit[1];
+    } 
+    return pageId;
+}
+
 function drawSite(pageId){
+    // in case there is no pageId in the URL, pageId will be 0.
+    // when pageId is 0 we should loop through the pages and get the Id of the first one which has rootPage: true
     if (pageId == 0){
-        for (let i=0; i<data.pages.length; i++){
+        for (let i=0; i < data.pages.length; i++){
             if (data.pages[i].metaData.rootPage === true){
                 pageId = data.pages[i].id;
                 break; // stops the for loop at the first occurance. No need to loop through the rest of the pages
-        }
+            }
         }
     }
-    drawFooter();
-    drawNav(pageId);
-    drawPage(pageId);
+    drawFooter();       // call the drawFooter function to draw the footer content
+    drawNav(pageId);    // call the drawNav function to draw the navigation. Give (pageId) as a parameter to tell which page is the current page
+    drawPage(pageId);   // call the drawPage function to draw the page. Give (pageId) as a parameter to tell which page to draw
+}
+
+function drawFooter(){
+    // draw the footer
+    let footerString = `
+        <h4>Contact us:</h4>
+        <address id="address">${data.site.footer.address}</address>
+        <div id="email">${data.site.footer.email}</div>
+    `;
+    drawData("footer", footerString);
 }
 
 function drawNav(currentPageId){
+    // draw navigation items as links in an ul-li list
     let navString = "<ul>";
     for (let i=0; i < data.pages.length; i++){
         if (data.pages[i].metaData.visible){
@@ -200,11 +248,14 @@ function drawNav(currentPageId){
 }
 
 function drawPage(pageId){
+    // get a page object that fits pageId
     let page = findPageById(pageId);
-    drawTitle(page);
-
     
-    // get template
+    // draw the title, draw the content
+    drawTitle(page);
+    
+    
+    // figure out which template to use for this page
     const template = page.metaData.template;
     switch (template) {
         case "page":
@@ -214,26 +265,31 @@ function drawPage(pageId){
         drawTemplateNews(page);
         break;
     }
-    let templateClass = `template-${template}`;
+
     // add template type to body as css class
+    let templateClass = `template-${template}`;
     document.getElementsByTagName('body')[0].classList.add(templateClass);
 }
 
+function findPageById(pageId){
+    // go through the pages in the JSON object, find the page that matches the pageId and return the page object
+    let page;
+    for (let i=0;i < data.pages.length;i++){
+        if (data.pages[i].id == pageId) {
+            page = data.pages[i];
+            break; // stops the for loop at the first occurance. No need to loop through the rest of the pages
+        }
+    }
+    return page;
+}
+
 function drawTitle(page){
-    // Set title
+    // Set the title tag
     document.title = data.site.titlePrepend + page.metaData.title + data.site.titleAppend;
 }
 
-function drawFooter(){
-    let footerString = `
-        <h4>Contact us:</h4>
-        <address id="address">${data.site.footer.address}</address>
-        <div id="email">${data.site.footer.email}</div>
-    `;
-    drawData("footer", footerString);
-}
-
 function drawTemplatePage(page){
+    // draw a normal page
     let content = `
         <article>
             <h1>${page.content.header}</h1>
@@ -246,6 +302,7 @@ function drawTemplatePage(page){
 }
 
 function drawTemplateNews(page){
+    // draw a news page
     let content = `
         <h1>${page.content.header}</h1>
         <img class="news-image" src="${page.content.image}" alt="${page.content.header}">
@@ -257,8 +314,9 @@ function drawTemplateNews(page){
 }
 
 function newsList(){
+    // return all news objects as articles in a html string
     let newsListString = "<h2>Latest news</h2>";
-    for (let i=0;i<data.news.length;i++){
+    for (let i=0;i < data.news.length;i++){
         let item = data.news[i];
         newsListString += `
             <article class="newsItem">
@@ -277,28 +335,8 @@ function newsList(){
     return newsListString;
 }
 
-function findPageById(pageId){
-    let page;
-    for (let i=0;i<data.pages.length;i++){
-        if (data.pages[i].id == pageId) {
-            page = data.pages[i]
-            break; // stops the for loop at the first occurance. No need to loop through the rest of the pages
-        }
-    }
-    return page;
-}
-
 function drawData(elementId, newContent){
     // elementId is where we want the data, newContent is the new data
     document.getElementById(elementId).innerHTML = newContent;
 }
 
-function getPageIdFromUrl(){
-    let pageId = 0;
-    let url = window.location.href;
-    if(url.indexOf("pageId") > -1){
-        let ulrSplit = url.split("?pageId=");
-        pageId = ulrSplit[1];
-    } 
-    return pageId;
-}
